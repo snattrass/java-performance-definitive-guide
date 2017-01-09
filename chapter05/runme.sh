6feea502ed054985e87d42e03e4607140fbabf9d
Simon Nattrass <simon.nattrass@avaloq.com>
2017-01-09 17:55:48 +0000

    Move Paths to Variables
    Small tweaks to the path structure to make it a little easier when
    working across OSes.

--- chapter05/runme.sh
+++ chapter05/runme.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-CLASSPATH=/Users/simon/.m2/repository/javax/javaee-api/7.0/javaee-api-7.0.jar
-CLASSPATH=$CLASSPATH:../stock-api/target/stock-api-1.0-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:../stock-impl/target/stock-impl-1.0-SNAPSHOT.jar
-CLASSPATH=$CLASSPATH:../stock-batching/target/stock-batching-1.0-SNAPSHOT.jar
+#MAVEN_DIR=/Users/simon/.m2/
+MAVEN_DIR=/c/dev/bin/maven/.m2
+PROJECT_DIR=/c/dev/workspace/books/java-performance-definitive-guide
 
+CLASSPATH=$MAVEN_DIR/repository/javax/javaee-api/7.0/javaee-api-7.0.jar
+CLASSPATH=$CLASSPATH:$PROJECT_DIR/stock-api/target/stock-api-1.0-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:$PROJECT_DIR/stock-impl/target/stock-impl-1.0-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:$PROJECT_DIR/stock-batching/target/stock-batching-1.0-SNAPSHOT.jar
+
+echo $CLASSPATH
 echo "CMS GC"
 JVM_OPTS="-Xms2g -Xmx2g -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+TieredCompilation"
-time java $JVM_OPTS -DMockEntityManager=MockEntityManager -cp $CLASSPATH net.sdo.StockPriceHistoryBatcher 50000 1/1/01 12/31/06 0 100 > /dev/null
+time java $JVM_OPTS -DMockEntityManager=MockEntityManager -cp $CLASSPATH net.sdo.StockPriceHistoryBatcher 10000 1/1/01 12/31/06 0 100 > /dev/null
 
 echo "Throughput"
 JVM_OPTS="-Xms2g -Xmx2g -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+TieredCompilation"
-time java $JVM_OPTS -DMockEntityManager=MockEntityManager -cp $CLASSPATH net.sdo.StockPriceHistoryBatcher 50000 1/1/01 12/31/06 0 100 > /dev/null
+time java $JVM_OPTS -DMockEntityManager=MockEntityManager -cp $CLASSPATH net.sdo.StockPriceHistoryBatcher 10000 1/1/01 12/31/06 0 100 > /dev/null
 
 echo "G1"
 JVM_OPTS="-Xms2g -Xmx2g -XX:+UseG1GC -XX:+TieredCompilation"
-time java $JVM_OPTS -DMockEntityManager=MockEntityManager -cp $CLASSPATH net.sdo.StockPriceHistoryBatcher 50000 1/1/01 12/31/06 0 100 > /dev/null
+time java $JVM_OPTS -DMockEntityManager=MockEntityManager -cp $CLASSPATH net.sdo.StockPriceHistoryBatcher 10000 1/1/01 12/31/06 0 100 > /dev/null
