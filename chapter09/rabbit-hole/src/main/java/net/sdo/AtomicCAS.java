5d5c6f9f62a9303a930c6852104202d46d10cbfa
Simon Nattrass <simon.nattrass@avaloq.com>
2017-02-16 12:00:32 +0000

    Adding playground for Atomic to ASM.

---
+++ chapter09/rabbit-hole/src/main/java/net/sdo/AtomicCAS.java
@@ -0,0 +1,10 @@
+package net.sdo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicCAS {
+  private AtomicInteger atomicInteger;
+
+  public static void main (String[] args) {
+  }
+}
